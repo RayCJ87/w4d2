8932576bbe07a0b0d32c20179cee308eb37f6d59
RayCJ87 <shihcr.ke@gmail.com>
2018-12-18 18:43:55 +0000

    refactor finished

--- lookup_people.js
+++ lookup_people.js
@@ -12,18 +12,45 @@ const client = new pg.Client({
   ssl     : settings.ssl
 });
 
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
+
+client.connect();
+
 
   client.query("SELECT first_name, last_name, birthdate from famous_people where first_name = $1", input, (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    // console.log(result.rows[0]);
-    console.log("the ans: ", result.rows);
+    console.log(`Found ${result.rows.length} person(s) by the name ${input[0]}`);
+    for (let i = 0; i < result.rows.length; i++){
+      console.log(`- ${i+1}: ${result.rows[i].first_name} ${result.rows[i].last_name}, born ${result.rows[i].birthdate.toDateString()}`);
+    }
     client.end();
-  })
-})
+  });
+
+// console.log(peopleByName("Paul"));
+// client.connect((err) => {
+//   if (err) {
+//     return console.error("Connection Error", err);
+//   }
+
+  // client.query("SELECT first_name, last_name, birthdate from famous_people where first_name = $1", input, (err, result) => {
+  //   if (err) {
+  //     return console.error("error running query", err);
+  //   }
+  //   console.log(`Found ${result.rows.length} person(s) by the name ${input[0]}`);
+  //   for (let i = 0; i < result.rows.length; i++){
+  //     console.log(`- ${i+1}: ${result.rows[i].first_name} ${result.rows[i].last_name}, born ${result.rows[i].birthdate}`);
+  //   }
+//     // console.log(result.rows[0]);
+//     // console.log("the ans: ", result.rows);
+//     client.end();
+//   });
+// });
+
+
+// module.exports = {
+//   // peopleByName,
+//   printResult,
+//   close: () => {client.end(); }
+// };
 
